745f9fa53c2add9fb2eb659f015c38c2b554e2d0
Lana Avetikovi <86164298+Lanito@users.noreply.github.com>
2026-07-16 14:50:49 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Вставь сюда данные своего проекта Supabase
 // Найти их: Supabase → твой проект → Project Settings → API
-window.SUPABASE_URL = "https://drnvklmkfocauhthyvyi.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://drnvklmkfocauhthyvyi.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_YA06vMNQq-xmQmkWagbDEg_NB3xdXtU";
